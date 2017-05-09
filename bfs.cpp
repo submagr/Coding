6bc27255ca826977fc50e5454d9170dbe3c985fa
submagr <agshubh191@gmail.com>
2017-05-09 23:46:58 +0530

    May Challenge initialized

--- bfs.cpp
+++ bfs.cpp
@@ -1,65 +1,84 @@
-#include<bits/stdc++.h>
-#include<stdio.h>
+#include<iostream>
+#include <list>
+ 
 using namespace std;
-
-typedef struct node{
-	int value;
-	int distance;
-	bool visited;
-	struct node *next;
-}node;
-
-void bfs(vector < vector<node> > *graph, queue <int>*q, vector <int> *distance, vector<bool> *visited){
-	while(!q->empty()){
-		int s = q->back();
-		q->pop();
-		for(int i=0; i<(*graph)[s].size(); i++){
-			int neighbor = (*graph)[s][i].value;
-			if(!(*visited)[neighbor]){
-				(*visited)[neighbor] = true;
-				(*distance)[neighbor] = (*distance)[s] + 1;
-				q->push(neighbor);
-			}
-		}
-	}
-	return;
+ 
+// This class represents a directed graph using adjacency list representation
+class Graph
+{
+    int V;    // No. of vertices
+    list<int> *adj;    // Pointer to an array containing adjacency lists
+public:
+    Graph(int V);  // Constructor
+    void addEdge(int v, int w); // function to add an edge to graph
+    void BFS(int s);  // prints BFS traversal from a given source s
+};
+ 
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
 }
-int main(){
-	int n, e;
-	printf("Number of nodes = ");
-	scanf("%d", &n);
-	vector< vector<node> > graph(n); // Adjacency List
-	printf("Number of edges = ");
-	scanf("%d", &e);
-	for(int i=0; i<e; i++){
-		int n1, n2;
-		scanf("%d %d", &n1, &n2);
-		node temp;	
-		temp.value = n2;
-		temp.distance = 0;
-		temp.visited = false;
-		graph[n1].push_back(temp);
-		// Undirected Graph
-		temp.value = n1;
-		temp.distance = 0;
-		temp.visited = false;
-		graph[n2].push_back(temp);
-	}
-	// BFS
-	int s;
-	printf("Enter start node ")	;
-	// Create a visited array
-	vector <bool> visited(n, false);
-	vector <int> distance(n, -1);
-	scanf("%d", &s);
-	visited[s] = true;
-	distance[s] = 0;
-	queue <int>q;
-	q.push(s);
-	bfs(&graph, &q, &distance, &visited);
-	for(int i=0; i<distance.size(); i++){
-		printf("%d ", distance[i]);
-	}
-	printf("\n");
-	return 0; 
+ 
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w); // Add w to v's list.
+    adj[w].push_back(v); // Add w to v's list.
+}
+ 
+void Graph::BFS(int s)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++)
+        visited[i] = false;
+ 
+    // Create a queue for BFS
+    list<int> queue;
+ 
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
+ 
+    // 'i' will be used to get all adjacent vertices of a vertex
+    list<int>::iterator i;
+ 
+    while(!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+ 
+        // Get all adjacent vertices of the dequeued vertex s
+        // If a adjacent has not been visited, then mark it visited
+        // and enqueue it
+        for(i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            if(!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+}
+ 
+// Driver program to test methods of graph class
+int main()
+{
+    // Create a graph given in the above diagram
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+ 
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 2) \n";
+    g.BFS(2);
+ 
+    return 0;
 }
